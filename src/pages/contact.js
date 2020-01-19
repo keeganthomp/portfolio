2d2467ce09ffaff4b19ff55fe73260b6a9aa59fb
keeganthomp <whereiskeegan@gmail.com>
2020-01-18 18:23:50 -0800

    using react-icons for icons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import FA from 'react-fontawesome'
+import { FaLinkedin, FaPhone, FaMapMarkerAlt, FaEnvelope, FaGithub } from 'react-icons/fa'
+
+const SOCIAL_ICON_SIZE = '2.5rem'
 
 const Contact = () => (
   <Layout>
@@ -12,25 +14,25 @@ const Contact = () => (
       <div className='page-content_block'>
         <div className='contact-page_contact-block'>
           <div style={{ width: '2rem' }}>
-            <FA name='map-marker' />
+            <FaMapMarkerAlt />
           </div>
           <span>San Diego, CA.</span>
         </div>
         <div className='contact-page_contact-block'>
           <div style={{ width: '2rem' }}>
-            <FA name='envelope' />
+            <FaEnvelope />
           </div>
           <span>whereiskeegan@gmail.com</span>
         </div>
         <div className='contact-page_contact-block'>
           <div style={{ width: '2rem' }}>
-            <FA name='phone' onClick={() => console.log('WEEEE')} />
+            <FaPhone onClick={() => console.log('WEEEE')} />
           </div>
           <span>(334)618-9523</span>
         </div>
         <div className='contact-page_social-container'>
-          <FA name='linkedin' size='2x'/>
-          <FA name='github' size='2x'/>
+          <FaLinkedin size={SOCIAL_ICON_SIZE}/>
+          <FaGithub size={SOCIAL_ICON_SIZE}/>
         </div>
       </div>
     </div>
